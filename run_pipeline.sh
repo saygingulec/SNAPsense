6c0c22df64af1b5dfc10ab1f3f9ee08fd4330931
saygingulec <50466176+saygingulec@users.noreply.github.com>
2023-07-07 03:00:43 -0400

    Fixed bash script

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,6 +1,6 @@
 module load matlab/2022a
 
 for CELL_NAME in data/*; do
-  sbatch -t 1-0 --mem 16G --output="slurm-%j-${CELL_NAME##*/}.out" --wrap="matlab -batch \"SNAPsense_pipeline(\'${CELL_NAME}\')\""
+  sbatch -t 1-0 --mem 16G --output="slurm-%j-${CELL_NAME##*/}.out" --wrap="matlab -batch \"SNAPsense_pipeline('${CELL_NAME##*/}')\""
 done
 
